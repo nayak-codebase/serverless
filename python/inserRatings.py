0d3b0579f146ff7862214d7d0c8f06b1883933e3
nayak-codebase <praveen.nayak86@gmail.com>
2020-11-13 16:23:34 +0530

    Update inserRatings.py

--- python/inserRatings.py
+++ python/inserRatings.py
@@ -1,7 +1,6 @@
 import json
 import boto3
 from datetime import datetime
-#That's the lambda handler, you can not modify this method
 # the parameters from JSON body can be accessed like rating = event['rating']
 def lambda_handler(event, context):
     # Instanciating connection objects with DynamoDB using boto3 dependency
